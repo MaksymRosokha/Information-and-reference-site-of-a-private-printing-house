f2daf3025a99aa407268aead71976d4c1f60fe89
MaksymRosokha <maksimrosoha6@gmail.com>
2023-03-15 20:03:47 +0200

    Created the project and installed Laravel Mix.

---
+++ webpack.mix.js
@@ -0,0 +1,21 @@
+let mix = require('laravel-mix');
+let fs = require('fs');
+
+let getFiles = function (dir) {
+    return fs.readdirSync(dir).filter(file => {
+        return fs.statSync(`${dir}/${file}`).isFile();
+    });
+};
+
+getFiles('resources/css/').forEach(function (filePath) {
+    mix.css('resources/css/' + filePath, 'public/css');
+});
+
+
+getFiles('resources/scss/').forEach(function (filePath) {
+    mix.sass('resources/scss/' + filePath, 'public/css');
+});
+
+getFiles('resources/js/').forEach(function (filePath) {
+    mix.js('resources/js/' + filePath, 'public/js');
+});
