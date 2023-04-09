af09541f74e0879f27d35cb91ea01c562564a8ec
MaksymRosokha <maksimrosoha6@gmail.com>
2023-04-09 19:05:25 +0300

    Showed printing services on the main page of the website.

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -10,44 +10,55 @@
 
 @section('content')
     <section class="main">
-        <h2 class="main__title">Скарбничка нотаріуса. Видавництво ІВА</h2>
-        <article class="main__paragraphs">
-            <p class="main__paragraph paragraph">
+        <h2 class="main__title title">Скарбничка нотаріуса. Видавництво ІВА</h2>
+        <article class="main__about-company about-company">
+            <h3 class="about-company__title title">Про компанію</h3>
+
+            <p class="about-company__paragraph paragraph">
                 <strong class="paragraph__strong">Приватна друкарня</strong> - це компанія, яка надає послуги з
                 друкування різноманітної
                 продукції на замовлення. Це може бути все від візитних карток та листівок до брошур, книг, плакатів та
                 інших матеріалів рекламного характеру.
             </p>
-            <p class="main__paragraph paragraph">
+            <p class="about-company__paragraph paragraph">
                 Одна з головних переваг приватної друкарні полягає у тому, що вона може працювати
                 із специфічними запитами клієнтів та виконувати замовлення на індивідуальні проекти. Це означає, що
                 клієнти можуть отримати продукцію, яка повністю відповідає їхнім потребам та вимогам.
             </p>
-            <p class="main__paragraph paragraph">
+            <p class="about-company__paragraph paragraph">
                 Приватна друкарня також має більш гнучкий підхід до замовлень та
                 може працювати з клієнтами в більш індивідуальному порядку. Це може включати пропозиції щодо дизайну,
                 розміру та матеріалу продукції, а також консультації щодо технічних особливостей друкування.
             </p>
-            <p class="main__paragraph paragraph">
+            <p class="about-company__paragraph paragraph">
                 Звичайно, у друкарні можуть проводитись додаткові послуги, такі як ламінування, фальцювання, перфорація,
                 проклейка та інші операції з обробки та обробки продукції. Крім того, друкарня може працювати з різними
                 видами матеріалів, таких як папір, картон, пластик, тканини та інші.
             </p>
-            <p class="main__paragraph paragraph">
+            <p class="about-company__paragraph paragraph">
                 Завдяки приватній друкарні клієнти можуть отримати якісну продукцію від
                 професіоналів зі значним досвідом у галузі друкарства. Компанія використовує сучасні
                 технології та обладнання для виробництва продукції, що забезпечує високу якість та точність
                 друкування.
             </p>
-            <p class="main__paragraph paragraph">
+            <p class="about-company__paragraph paragraph">
                 Друкарня може також забезпечувати швидкість та ефективність виконання замовлень, що робить її корисною
                 для компаній, які потребують швидкого друкування більшого обсягу продукції. В друкарні працюють
                 кваліфіковані фахівці, які дбайливо виконують кожне замовлення з урахуванням всіх вимог клієнта та
                 стандартів якості.
             </p>
         </article>
-        <div class="main__everything-for-notary everything-for-notary">
-            <h3 class="everything-for-notary__title">Поліграфічні послуги</h3>
-        </div>
+        <section class="main__printing-services printing-services">
+            <h3 class="printing-services__title title">Поліграфічні послуги</h3>
+            <ol class="printing-services__list-of-services list-of-services">
+                @foreach($services as $service)
+                    <li class="everything-for-notary__service service">
+                        <a href="#" class="service__link">
+                            <img src="/storage/images/services/{{$service->image}}" alt="Зображення послуги {{ $service->name }}" class="service__image">
+                        </a>
+                    </li>
+                @endforeach
+            </ol>
+        </section>
     </section>
 @endsection
