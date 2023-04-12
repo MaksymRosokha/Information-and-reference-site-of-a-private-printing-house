cd11f57677dcd5208ca6ba0ba4757b124122bca7
MaksymRosokha <maksimrosoha6@gmail.com>
2023-04-12 16:47:08 +0300

    Styled the admin panel.

--- resources/views/users/admin.blade.php
+++ resources/views/users/admin.blade.php
@@ -9,6 +9,32 @@
 @endsection
 
 @section('content')
-    <h2>Admin</h2>
-    <a href="{{ route('logout') }}">Вихід</a>
+    <section class="admin">
+        <h2 class="admin__title">Адміністративна панель</h2>
+        <section class="admin__opportunities opportunities">
+            <table class="opportunities__table-of-opportunities table-of-opportunities">
+                <tr class="table-of-opportunities__row">
+                    <th class="table-of-opportunities__column table-of-opportunities__column--title">Послуги</th>
+                    <th class="table-of-opportunities__column table-of-opportunities__column--title">Продукти</th>
+                    <th class="table-of-opportunities__column table-of-opportunities__column--title">Новини</th>
+                </tr>
+                <tr class="table-of-opportunities__row">
+                    <td class="table-of-opportunities__column table-of-opportunities__column--create">Створити</td>
+                    <td class="table-of-opportunities__column table-of-opportunities__column--create">Створити</td>
+                    <td class="table-of-opportunities__column table-of-opportunities__column--create">Створити</td>
+                </tr>
+                <tr class="table-of-opportunities__row">
+                    <td class="table-of-opportunities__column table-of-opportunities__column--update">Редагувати</td>
+                    <td class="table-of-opportunities__column table-of-opportunities__column--update">Редагувати</td>
+                    <td class="table-of-opportunities__column table-of-opportunities__column--update">Редагувати</td>
+                </tr>
+                <tr class="table-of-opportunities__row">
+                    <td class="table-of-opportunities__column table-of-opportunities__column--delete">Видалити</td>
+                    <td class="table-of-opportunities__column table-of-opportunities__column--delete">Видалити</td>
+                    <td class="table-of-opportunities__column table-of-opportunities__column--delete">Видалити</td>
+                </tr>
+            </table>
+        </section>
+        <a class="admin__logout" href="{{ route('logout') }}">Вихід</a>
+    </section>
 @endsection
