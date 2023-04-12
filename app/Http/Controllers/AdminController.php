625f0e21a8bdd130884dc88591d9a259739c870e
MaksymRosokha <maksimrosoha6@gmail.com>
2023-04-12 18:54:25 +0300

    Redid style for admin page.

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -8,4 +8,40 @@ class AdminController extends Controller
     {
         return view('users.admin');
     }
+
+    public function createService()
+    {
+    }
+
+    public function updateService()
+    {
+    }
+
+    public function deleteService()
+    {
+    }
+
+    public function createProduct()
+    {
+    }
+
+    public function updateProduct()
+    {
+    }
+
+    public function deleteProduct()
+    {
+    }
+
+    public function createPost()
+    {
+    }
+
+    public function updatePost()
+    {
+    }
+
+    public function deletePost()
+    {
+    }
 }
--- resources/views/users/admin.blade.php
+++ resources/views/users/admin.blade.php
@@ -12,73 +12,54 @@
     <section class="admin">
         <h2 class="admin__title">Адміністративна панель</h2>
         <section class="admin__opportunities opportunities">
-            <table class="opportunities__table-of-opportunities table-of-opportunities">
-                <tr class="table-of-opportunities__row">
-                    <th class="table-of-opportunities__column table-of-opportunities__column--title">Послуги</th>
-                    <th class="table-of-opportunities__column table-of-opportunities__column--title">Продукти</th>
-                    <th class="table-of-opportunities__column table-of-opportunities__column--title">Новини</th>
-                </tr>
-                <tr class="table-of-opportunities__row">
-                    <td class="table-of-opportunities__column table-of-opportunities__column--create">
-                        <button-with-modal-window class="opportunities__button"
-                                                  button-text="Створити послугу">
+            <ul class="opportunities__list-of-opportunities list-of-opportunities">
+                <li class="list-of-opportunities__block">
+                    <strong class="list-of-opportunities__title">Послуги</strong>
+                    <button-with-modal-window class="list-of-opportunities__button"
+                                              button-text="Створити послугу">
 
-                        </button-with-modal-window>
-                    </td>
-                    <td class="table-of-opportunities__column table-of-opportunities__column--create">
-                        <button-with-modal-window class="opportunities__button"
-                                                  button-text="Створити продукт">
+                    </button-with-modal-window>
+                    <button-with-modal-window class="list-of-opportunities__button"
+                                              button-text="Редагувати послугу">
 
-                        </button-with-modal-window>
-                    </td>
-                    <td class="table-of-opportunities__column table-of-opportunities__column--create">
-                        <button-with-modal-window class="opportunities__button"
-                                                  button-text="Створити пост">
+                    </button-with-modal-window>
+                    <button-with-modal-window class="list-of-opportunities__button"
+                                              button-text="Видалити послугу">
 
-                        </button-with-modal-window>
-                    </td>
-                </tr>
-                <tr class="table-of-opportunities__row">
-                    <td class="table-of-opportunities__column table-of-opportunities__column--update">
-                        <button-with-modal-window class="opportunities__button"
-                                                  button-text="Редагувати послугу">
+                    </button-with-modal-window>
+                </li>
+                <li class="list-of-opportunities__block">
+                    <strong class="list-of-opportunities__title">Продукти</strong>
 
-                        </button-with-modal-window>
-                    </td>
-                    <td class="table-of-opportunities__column table-of-opportunities__column--update">
-                        <button-with-modal-window class="opportunities__button"
-                                                  button-text="Редагувати продукт">
+                    <button-with-modal-window class="list-of-opportunities__button"
+                                              button-text="Створити продукт">
 
-                        </button-with-modal-window>
-                    </td>
-                    <td class="table-of-opportunities__column table-of-opportunities__column--update">
-                        <button-with-modal-window class="opportunities__button"
-                                                  button-text="Редагувати пост">
+                    </button-with-modal-window>
+                    <button-with-modal-window class="list-of-opportunities__button"
+                                              button-text="Редагувати продукт">
 
-                        </button-with-modal-window>
-                    </td>
-                </tr>
-                <tr class="table-of-opportunities__row">
-                    <td class="table-of-opportunities__column table-of-opportunities__column--delete">
-                        <button-with-modal-window class="opportunities__button"
-                                                  button-text="Видалити послугу">
+                    </button-with-modal-window>
+                    <button-with-modal-window class="list-of-opportunities__button"
+                                              button-text="Видалити продукт">
 
-                        </button-with-modal-window>
-                    </td>
-                    <td class="table-of-opportunities__column table-of-opportunities__column--delete">
-                        <button-with-modal-window class="opportunities__button"
-                                                  button-text="Видалити продукт">
+                    </button-with-modal-window>
+                </li>
+                <li class="list-of-opportunities__block">
+                    <strong class="list-of-opportunities__title">Новини</strong>
+                    <button-with-modal-window class="list-of-opportunities__button"
+                                              button-text="Створити пост">
 
-                        </button-with-modal-window>
-                    </td>
-                    <td class="table-of-opportunities__column table-of-opportunities__column--delete">
-                        <button-with-modal-window class="opportunities__button"
-                                                  button-text="Видалити пост">
+                    </button-with-modal-window>
+                    <button-with-modal-window class="list-of-opportunities__button"
+                                              button-text="Редагувати пост">
 
-                        </button-with-modal-window>
-                    </td>
-                </tr>
-            </table>
+                    </button-with-modal-window>
+                    <button-with-modal-window class="list-of-opportunities__button"
+                                              button-text="Видалити пост">
+
+                    </button-with-modal-window>
+                </li>
+            </ul>
         </section>
         <a class="admin__logout" href="{{ route('logout') }}">Вихід</a>
     </section>
